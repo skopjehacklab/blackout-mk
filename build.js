579a83872e274f5043eee3a17ae80280af841dd4
Gorgi Kosev <gorgi.kosev@gmail.com>
2012-10-08 00:37:56 +0200

    completed template engine

--- build.js
+++ build.js
@@ -1,19 +1,20 @@
 var fs = require('fs'),
     argv = require('optimist').argv;
 
+var ishtml = /\.html?$/,
+    templatePath = argv._[0];
 
-fs.readFile(argv._[0], 'utf8', function(err, tmpl) {
-    var ms = tmpl.match(/\$\$([-_\s.0-9a-zA-Z]+)\$\$/g)
+fs.readFile(templatePath, 'utf8', function(err, template) {
+    var ms = template.match(/\$\$([-_\s.0-9a-zA-Z]+)\$\$/g)
     var remaining = ms.length;
     ms.forEach(function(m) {
-        var tmplFile = m.match(/\$\$(.+)\$\$/)[1];
-        var tmplHtml = /\.html?$/.test(tmplFile);
-        fs.readFile(tmplFile, 'utf8', function(err, tmplFile) {
+        var partialPath = m.match(/\$\$(.+)\$\$/)[1];
+        fs.readFile(partialPath, 'utf8', function(err, partial) {
             if (err) console.error(err);
-            if (tmplHtml) tmplFile = tmplFile.replace(/[\r\n]+/g, " ");
-            tmpl = tmpl.replace(m, tmplFile);
-            if (!--remaining) process.stdout.write(tmpl);
-            else console.error(remaining);
+            if (ishtml.test(partialPath) && !ishtml.test(templatePath)) 
+                partial = partial.replace(/[\r\n]+/g, " ");
+            template = template.replace(m, partial);
+            if (!--remaining) process.stdout.write(template);
         });
     });
 });
