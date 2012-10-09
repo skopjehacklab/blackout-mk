26bee471e475e30862cc391e77ddf8be47470d18
Damjan Georgievski <gdamjan@gmail.com>
2012-10-09 12:16:45 +0200

    New build of the files

--- blackout-mk.js
+++ blackout-mk.js
@@ -1,5 +1,5 @@
 (function (){
-    var blackoutHtmlString = '<div class="blackout-script-div" style="color:#bbb; margin: auto; max-width:45em; font-family: Georgia, serif !important; font-size: 16px;">     <h1 style="font-size:1.5em; background-color:transparent; border:none; color:#bbb;">Вака ќе изгледа македонскиот интернет ако се воведе новиот закон          за одговорност за навреда и клевета</h1>          <p style="color:#bbb;">Дознајте повеќе:     <a style="color:#8cf;" href="http://skopjehacklab.github.com/blackout-mk/more.html">Што значи ова?</a>     </p>     <p style="color:#bbb;">Контактирај ги надлежните:     <a style="color:#8cf;" href="http://www.sobranie.mk/ext/contact.aspx">         sobranie.mk     </a>     <img src="http://status.spodeli.org/log?tag=test" style="width:1px; height:1px;">     </p> </div> ';
+    var blackoutHtmlString = '<div class="blackout-script-div" style="color:#bbb; margin: auto; max-width:45em; font-family: Georgia, serif !important; font-size: 16px;">     <h1 style="font-size:1.5em; background-color:transparent; border:none; color:#bbb;">&#1042;&#1072;&#1082;&#1072; &#1116;&#1077; &#1080;&#1079;&#1075;&#1083;&#1077;&#1076;&#1072; &#1084;&#1072;&#1082;&#1077;&#1076;&#1086;&#1085;&#1089;&#1082;&#1080;&#1086;&#1090; &#1080;&#1085;&#1090;&#1077;&#1088;&#1085;&#1077;&#1090; &#1072;&#1082;&#1086; &#1089;&#1077; &#1074;&#1086;&#1074;&#1077;&#1076;&#1077; &#1085;&#1086;&#1074;&#1080;&#1086;&#1090; &#1079;&#1072;&#1082;&#1086;&#1085;          &#1079;&#1072; &#1086;&#1076;&#1075;&#1086;&#1074;&#1086;&#1088;&#1085;&#1086;&#1089;&#1090; &#1079;&#1072; &#1085;&#1072;&#1074;&#1088;&#1077;&#1076;&#1072; &#1080; &#1082;&#1083;&#1077;&#1074;&#1077;&#1090;&#1072;</h1>          <p style="color:#bbb;">&#1044;&#1086;&#1079;&#1085;&#1072;&#1112;&#1090;&#1077; &#1087;&#1086;&#1074;&#1077;&#1116;&#1077;:     <a style="color:#8cf;" href="http://skopjehacklab.github.com/blackout-mk/more.html">&#1064;&#1090;&#1086; &#1079;&#1085;&#1072;&#1095;&#1080; &#1086;&#1074;&#1072;?</a>     </p>     <p style="color:#bbb;">&#1050;&#1086;&#1085;&#1090;&#1072;&#1082;&#1090;&#1080;&#1088;&#1072;&#1112; &#1075;&#1080; &#1085;&#1072;&#1076;&#1083;&#1077;&#1078;&#1085;&#1080;&#1090;&#1077;:     <a style="color:#8cf;" href="http://www.sobranie.mk/ext/contact.aspx">         sobranie.mk     </a>     <img src="http://status.spodeli.org/log?tag=test" style="width:1px; height:1px;">     </p> </div> ';
 	var root = this;
 
 	var SopaBlackout = function(){};
@@ -108,7 +108,7 @@
 	SopaBlackout.VERSION = '0.2.0';
 	SopaBlackout.MIN_HEIGHT = 100;
 	SopaBlackout.HEADER_TEXT = "Ова е изгледот на македонскиот интернет под законот ЗАКОН_ОВДЕ";
-	SopaBlackout.CONTINUE_TEXT = "(кликни за да продолжиш) ";
+	SopaBlackout.CONTINUE_TEXT = "(&#1082;&#1083;&#1080;&#1082;&#1085;&#1080; &#1079;&#1072; &#1076;&#1072; &#1087;&#1088;&#1086;&#1076;&#1086;&#1083;&#1078;&#1080;&#1096;) ";
 	SopaBlackout.ZINDEX = Math.pow(2, 31) - 2;
 	SopaBlackout.DEFAULTS = {
 		'id': false,
